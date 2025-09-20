4d5c1a5d8d25ddb5aa1f695f5295a7d5b4b3c845
KeerthiPatil-RTL <keerthipatil2001@gmail.com>
2025-09-20 06:14:28 -0400

    Update DC.v

--- RCA/DC.v
+++ RCA/DC.v
@@ -1,1 +1,20 @@
+module FA(input a,b,cin , output sum,cout);
+assign sum = a ^ b ^ cin;
+assign cout = a & b | b & cin | cin & a; 
+endmodule
+
+
+module RCA(
+    input [3:0] A,
+    input [3:0] B,
+    input Cin,
+    output [3:0] SUM,
+    output Cout
+    );
+    wire C1,C2,C3;  
+   FA  fa0(.a(A[0]),.b(B[0]),.cin(Cin) ,.sum(SUM[0]),.cout(C1));
+   FA  fa1(.a(A[1]),.b(B[1]),.cin(C1) ,.sum(SUM[1]),.cout(C2));
+  FA   fa2(.a(A[2]),.b(B[3]),.cin(C2) ,.sum(SUM[2]),.cout(C3));
+   FA  fa3(.a(A[3]),.b(B[3]),.cin(C3) ,.sum(SUM[3]),.cout(Cout));
+endmodule
 
